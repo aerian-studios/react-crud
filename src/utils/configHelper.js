23d2f85844d3d2b11143184816e4538111e8db02
Simon Neaves <simon@aerian.com>
2017-02-14 08:13:03 +0000

    Blueprint URL from config

--- src/utils/configHelper.js
+++ src/utils/configHelper.js
@@ -29,6 +29,26 @@ const configHelper = {
         url += listUrlFromConfig ? listUrlFromConfig : '/' + entity
 
         return url
+    },
+
+    getBlueprintUrl(config, entity, entityId = null) {
+        //@todo validate input
+
+        //get base url from config
+        let url = config.get('baseUrl')
+        //@todo this is a bit of a hack because the config returns the root object if the property is a blank string
+        if (url !== null && typeof url === 'object') {
+            url = ''
+        }
+
+        //if a blueprintUrl is set in the config use it, otherwise default to '/' + entity + '/blueprint
+        let blueprintUrlFromConfig = config.get('entities').byId[entity].blueprintUrl
+        url += blueprintUrlFromConfig ? blueprintUrlFromConfig : '/' + entity + '/blueprint'
+        if (entityId) {
+            url += '/' + entityId;
+        }
+
+        return url
     }
 
 }
